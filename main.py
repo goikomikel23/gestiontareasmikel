a8143f73ff11f218c603f9f71ccc9f5d912aa443
goikomikel23 <EHU@MacBook-Pro-de-Mikel-2.local>
2016-05-14 11:25:06 +0200

    Formulario de Registro

--- main.py
+++ main.py
@@ -14,7 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from calendar import weekday
+
 import webapp2
+import cgi
 
 formLogin = '''
 <html>
@@ -22,32 +25,68 @@ formLogin = '''
     <form method="post">
         <h1 align="center">Welcome</h1>
         <p align="center">
-            User: <input type="text" name="user"/>
+            Login : <input type="text" name="login"/>
         <br/>
         Password: <input type="password" name="password"/>
         <br/>
         <input type="submit" value="Enviar"/>
         </p>
     </form>
-    <p align="center">
-        <a href='localhost:8080/register'>Register</a>
-    </p>
 </body>
 </html>
 '''
 
-register = ''' REGISTRATION '''
+formRegister = '''
+<html>
+<body>
+    <form action='/registerData' method="post">
+        <h1 align="center">Registration</h1>
+        <p align="center">
+            Email: <input type="text" name="email"/>
+        <br/>
+        Password: <input type="password" name="password"/>
+        <br/>
+        Repeat Password: <input type="password" name="password"/>
+        <br/>
+        Name: <input type="password" name="name"/>
+        <br/>
+        LastName: <input type="password" name="lastName"/>
+        <br/>
+        DNI: <input type="password" name="dni"/>
+        <br/>
+        <br/>
+        Recover password Question:
+        Secret Question: <input type="password" name="secretQ"/>
+        <br/>
+        Answer: <input type="password" name="answer"/>
+        <br/>
+        <input type="submit" value="Registrar"/>
+        </p>
+    </form>
+</body>
+</html>
+'''
+
+holacaracola = ''' holaaa '''
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write(formLogin)
+        self.response.out.write("<p align='Center'><a href='/register'>Register</a></p>")
 
-class Register(webapp2.RequestHandler):
+class RegisterForm(webapp2.RequestHandler):
     def get(self):
-        self.response.write(register)
+        self.response.write(formRegister)
+
+class RegisterData(webapp2.RequestHandler):
+    def post(self):
+        self.response.write('<!doctype html><html><body>You wrote:<pre>')
+        self.response.write(cgi.escape(self.request.get('email')))
+        self.response.write('</pre></body></html>')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/register', Register)
+    ('/register', RegisterForm),
+    ('/registerData', RegisterData)
 ], debug=True)
 
